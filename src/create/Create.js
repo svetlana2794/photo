58ebac0ed70e42e77fd4b4383bc3647724d2770e
svetlana2794 <sb270994@gmail.com>
2026-04-08 13:37:47 +0300

    value

--- src/create/Create.js
+++ src/create/Create.js
@@ -7,6 +7,8 @@ import "./styles.css";
 
 function Create() {
 	const size = [300, 400, 500, 600, 700, 800];
+	const [selectedWidth, setSelectedWidth] = useState(300);
+	const [selectedHeight, setSelectedHeight] = useState(300);
 
   return (
     <div id="create">
@@ -14,12 +16,16 @@ function Create() {
       <Form method="post">
         <label>Ширина: </label>
         <Dropdown
+        value={selectedWidth}
+  onChange={(e) => setSelectedWidth(e.value)}
   name="width"
   options={size}
 />
         <br />
         <label>Высота: </label>
         <Dropdown
+        value={selectedHeight}
+  onChange={(e) => setSelectedHeight(e.value)}
   name="height"
   options={size}
 />
