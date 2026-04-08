152275739c506f418d313618e2df15037eccc074
svetlana2794 <sb270994@gmail.com>
2026-04-08 13:20:06 +0300

    create2

--- src/create/Create.js
+++ src/create/Create.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form } from "react-router-dom";
-import { Dropdown } from 'primereact/dropdown';
+import { Dropdown } from 'dropdown.esm.js';
 
 
 import "./styles.css";
