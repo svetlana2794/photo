de34eb3ead5c52bf156266614250ce1c48f03d2c
svetlana2794 <sb270994@gmail.com>
2026-04-21 12:06:17 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,23 +39,13 @@ MiniCssExtractPlugin.loader,
 "css-loader",
 "sass-loader"
 	]
-	}
-]},/*
-optimization: {
-runtimeChunk: "single",
-splitChunks: {
-chunks: "all",
-maxInitialRequests: Infinity,
-minSize: 0,
-cacheGroups: {
-vendor: {
-test: /[\\/]node_modules[\\/]/,
-name(module) {
-const packageName=module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-return `npm.${packageName.replace('@', '')}`;
-}
-}
-}
-}
-},*/
+	},
+	{
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]',
+        },
+      },
+]}
 }
